4450219ac9beeee9c655371acca2f6cfa2a449cd
rmCoder7 <mohammedayon2021@gmail.com>
2023-08-22 00:27:30 +0600

    first commit

---
+++ for.js
@@ -0,0 +1,26 @@
+// for(var odd=1; odd<=10; odd=odd+2)
+// {
+//   console.log(odd);
+// }
+
+// var arry = [20,30,40,50,60,70,80,90];
+
+// for(var i = 0 ; i <8; i++)
+// {
+//  console.log(i)
+// }
+// var arry = [20,30,40,50,60,70,80,90];
+
+// for (var i =0 ; i <8; i++)
+// {
+//     var array=arry[i]
+//     console.log(array)
+// }
+
+
+function myFunction(a, b)
+{
+    return a*b
+}
+
+let x = myFunction(4,3)
---
+++ while.js
@@ -0,0 +1,35 @@
+// var roastGiven = 0;
+
+// while (roastGiven< 7)
+// {
+//     // console.log("roastGiven den");
+//     console.log(roastGiven);
+//     roastGiven++;
+// }
+
+// var number = 0;
+
+// while(number <10)
+// {
+//   number++
+//   console.log(number)
+// }
+
+// var even = 0;
+
+// while(even <=10)
+// {
+//   even = even + 2;
+//   console.log(even)
+// }
+
+
+// var odd = 1;
+
+// while(odd <=10)
+// {
+//   odd = odd + 2;
+//   console.log(odd)
+// }
+
+
